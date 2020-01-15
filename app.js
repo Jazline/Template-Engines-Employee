0c216c8ea4f44d59d977738e57eb2cb017ef1a0c
Jazline <jazlinekeli@gmail.com>
2020-01-14 22:33:57 -0600

    uploading hw

---
+++ app.js
@@ -0,0 +1,6 @@
+const Manager = require("./lib/manager");
+const Employee = require("./lib/employee");
+const Intern = require("./lib/intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
